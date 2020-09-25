b2cf83229864d72f7e1e6bb24ece82132c32ab73
Zach Gardner <gardz@amazon.com>
2020-09-25 10:26:36 -0400

    Update bulkinsert.py

--- bulkinsert.py
+++ bulkinsert.py
@@ -6,44 +6,41 @@ import redis
 import time
 import multiprocessing as mp
 
-class InsertWorker(Thread):
-    def __init__(self, queue):
-        Thread.__init__(self)
-        self.queue = queue
-def run(records):
+def run(records,start_time):
     r = redis.Redis(host="localhost")
-    loop_start_time = time.time()
     rp = r.pipeline(transaction=False)
     for row in records:
-        emplid="Emp"+str(row[0])
+        emplid="Emp"+str(row[0]) 
         rp.hmset(emplid,{"x":"wklfjfejwfojefwiefoiewoiefoweif","y":"kfhjqwleifhewiqofuneqwifbefil","z":"iowjpoiefjpweofijpqwoefijpiofwejoiwe","a":"wklfjfejwfojefwiefoiewoiefoweif","b":"kfhjqwleifhewiqofuneqwifbefil"})
     rp.execute()
-    #print("--- %s seconds for loop ---" % (time.time() - loop_start_time))
+    print("---Worker done %s seconds for loop ---" % (time.time() - start_time))
 
-def lambda_handler(event, context):
-    print("starting")
+
+def run_migration():
+    print("starting, # of cpus " + str(mp.cpu_count()))
     conn = psycopg2.connect(database="postgres",user = "postgres", password = '', host = "localhost", port = "5432")
     print("connected to PG")
     queue = mp.Queue()
     procs = []
     proc = mp.Process(target=run)
     procs.append(proc)
-    port = '6379'
-    chunk_size = 1000
-    cur = conn.cursor(name="my_cursor_name")
+    port = '6379'    
+    chunk_size = 10000
+    cur = conn.cursor(name="my_cursor_name")  
     query = "select * from pgbench_accounts"
     cur.execute(query)
     start_time = time.time()
     while True:
         records = cur.fetchmany(size=chunk_size)
+        loop_start_time = time.time()
         if not records:
             break
-        proc = mp.Process(target=run, args=(records,))
+        proc = mp.Process(target=run, args=(records,start_time,))
         procs.append(proc)
         proc.start()
     cur.close()
     conn.close()
 
     print("---Script done,  %s seconds ---" % (time.time() - start_time))
-
-lambda_handler(1,1)
+    
+run_migration()
