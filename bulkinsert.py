ec147a78fc85f52f63a0694dc247a8e0f25770e8
Zach Gardner <gardz@amazon.com>
2020-09-25 10:32:30 -0400

    Update bulkinsert.py

--- bulkinsert.py
+++ bulkinsert.py
@@ -11,25 +11,31 @@ def run(records,start_time):
     rp = r.pipeline(transaction=False)
     for row in records:
         emplid="Emp"+str(row[0]) 
+        #sample data
         rp.hmset(emplid,{"x":"wklfjfejwfojefwiefoiewoiefoweif","y":"kfhjqwleifhewiqofuneqwifbefil","z":"iowjpoiefjpweofijpqwoefijpiofwejoiwe","a":"wklfjfejwfojefwiefoiewoiefoweif","b":"kfhjqwleifhewiqofuneqwifbefil"})
     rp.execute()
     print("---Worker done %s seconds for loop ---" % (time.time() - start_time))
 
 
 def run_migration():
-    print("starting, # of cpus " + str(mp.cpu_count()))
+    print("starting migration: # of cpus " + str(mp.cpu_count()))
     conn = psycopg2.connect(database="postgres",user = "postgres", password = '', host = "localhost", port = "5432")
     print("connected to PG")
+    
+    #mp set up
     queue = mp.Queue()
     procs = []
     proc = mp.Process(target=run)
     procs.append(proc)
-    port = '6379'    
+    
+    #records per cursor fetch
     chunk_size = 10000
     cur = conn.cursor(name="my_cursor_name")  
     query = "select * from pgbench_accounts"
     cur.execute(query)
     start_time = time.time()
+    
+    #fetch cursor records and put into multiprocessing queue
     while True:
         records = cur.fetchmany(size=chunk_size)
         loop_start_time = time.time()
@@ -38,6 +44,8 @@ def run_migration():
         proc = mp.Process(target=run, args=(records,start_time,))
         procs.append(proc)
         proc.start()
+    
+    #clean up
     cur.close()
     conn.close()
 
